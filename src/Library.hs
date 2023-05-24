f974644fd8a214603d65af49736a84a718d0d6d8
ThiagoGonzalezz <thgonzalez@frba.utn.edu.ar>
2023-05-24 14:23:31 -0300

    Parcial de practica terminado

--- src/Library.hs
+++ src/Library.hs
@@ -3,6 +3,12 @@ import PdePreludat
 
 type Enfermedad = String
 
+type Hierbas = (Raton->Raton)
+
+type Medicamento = [Hierbas]
+
+type Comunidad = [Raton]
+
 data Raton = UnRaton {
     nombre::String,
     edad::Number,
@@ -40,8 +46,59 @@ huesudo = UnRaton "Huesudo" 4 10 [obesidad, sinusitis]
 hierbaBuena::Raton->Raton
 hierbaBuena ratoncito = ratoncito{edad = sqrt (edad ratoncito)}
 
-hiervaVerde::Raton->String->Raton
-hiervaVerde raton terminacion = raton{ enfermedades = filter (noTerminaCon terminacion) (enfermedades raton)} 
+hierbaVerde::String->Raton->Raton
+hierbaVerde terminacion raton  = raton{ enfermedades = filter (not . terminaCon terminacion) (enfermedades raton)} 
 
-noTerminaCon::String->Enfermedad->Bool
-noTerminaCon terminacion
+terminaCon::String->Enfermedad->Bool
+terminaCon terminacion enfermedad = drop (length enfermedad - length terminacion) enfermedad == terminacion
+
+alcachofa::Raton->Raton
+alcachofa raton 
+    |peso raton > 2 = raton{peso = 0.9 * peso raton}
+    |otherwise =  raton{peso = 0.95 * peso raton}
+
+hierbaZort::Raton->Raton
+hierbaZort raton = raton{edad = 0, enfermedades = []}
+
+hierbaDelDiablo :: Raton -> Raton
+hierbaDelDiablo raton = raton {peso = max 0 (peso raton - 0.1), enfermedades = filter mas10Letras (enfermedades raton)}
+
+mas10Letras::Enfermedad->Bool
+mas10Letras enfermedad = length enfermedad >= 10
+
+--3
+administrarMedicamentos:: [Medicamento] -> Raton -> Raton
+administrarMedicamentos medicamentos raton = foldl (foldr ($)) raton medicamentos 
+
+pondsAntiAge::Medicamento
+pondsAntiAge = [hierbaBuena, hierbaBuena, hierbaBuena, alcachofa]
+
+reduceFatFast:: Number -> Medicamento
+reduceFatFast potencia = hierbaVerde "Obesidad" : replicate potencia alcachofa
+
+pdepCilina :: Medicamento
+pdepCilina = [hierbaVerde (head sufijosInfecciosas) , hierbaVerde (sufijosInfecciosas !! 1), hierbaVerde (sufijosInfecciosas !! 2), hierbaVerde (last sufijosInfecciosas)] 
+
+sufijosInfecciosas = [ "sis", "itis", "emia", "cocos"]
+
+--4
+--a
+cantidadIdeal::(Number->Bool) -> Number
+cantidadIdeal condicion =  buscarCualCumple condicion [1 ..]
+
+buscarCualCumple::(Number->Bool)->[Number]->Number
+buscarCualCumple condicion (x:xs) 
+    |condicion x = x
+    |otherwise = buscarCualCumple condicion xs
+
+--b
+lograEstabilizar::Comunidad->Medicamento->Bool
+lograEstabilizar comunidad medicamento  = (all estabilizo . map (administrarMedicamentos [medicamento])) comunidad 
+
+estabilizo::Raton->Bool
+estabilizo raton = peso raton < 1 && length (enfermedades raton) < 3
+
+--c
+
+potenciaIdeal::Comunidad->Number
+potenciaIdeal comunidad = cantidadIdeal (lograEstabilizar  comunidad . reduceFatFast) 
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,42 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
-
+  describe "Hierbas" $ do
+    it "hierbaBuena" $ do
+      hierbaBuena huesudo `shouldBe` UnRaton {nombre = "Huesudo", edad = 2, peso = 10, enfermedades = ["Obesidad","Sinusitis"]}
+    it "hierbaVerde" $ do
+      hierbaVerde "dad" huesudo `shouldBe` UnRaton {nombre = "Huesudo", edad = 4, peso = 10, enfermedades = ["Sinusitis"]}
+    it "alcachofa > 2 kilos" $ do
+      alcachofa huesudo `shouldBe` UnRaton "Huesudo" 4 9 [obesidad, sinusitis]
+    it "alcachofa < 2 kilos" $ do
+      alcachofa cerebro `shouldBe` UnRaton {nombre = "Cerebro", edad = 9, peso = 0.19, enfermedades = ["Brucelosis","Sarampion","Tuberculosis"]}
+    it "hierbaZort" $ do
+      hierbaZort huesudo `shouldBe` UnRaton "Huesudo" 0 10 []
+    it "hierbaDelDiablo peso > 0" $ do
+      hierbaDelDiablo cerebro `shouldBe` UnRaton "Cerebro" 9 0.1 [brucelosis, tuberculosis] 
+    it "hierbaDelDiablo peso = 0" $ do
+      hierbaDelDiablo (UnRaton "Huesudo" 4 0 [obesidad, sinusitis]) `shouldBe`  UnRaton "Huesudo" 4 0 []
+  
+  describe "Medicamentos" $ do
+    it "reduceFatFast" $ do
+      administrarMedicamentos [pondsAntiAge] bincenterrata `shouldBe` UnRaton "Bicenterrata" 2 0.19 []
+    it "reduceFatFast 1" $ do
+      administrarMedicamentos [reduceFatFast 1] huesudo `shouldBe` UnRaton "Huesudo" 4 9 [sinusitis]
+    it "reduceFatFast 2" $ do
+      administrarMedicamentos [reduceFatFast 2] huesudo `shouldBe` UnRaton "Huesudo" 4 8.1 [sinusitis]
+    it "pdepCilina" $ do
+      administrarMedicamentos [pdepCilina] huesudo `shouldBe` UnRaton "Huesudo" 4 10 [obesidad]
+  
+  describe "Experimentos" $ do
+    it "cantidadIdeal even" $ do
+      cantidadIdeal even `shouldBe` 2
+    it "cantidadIdeal even" $ do
+      cantidadIdeal (>7) `shouldBe` 8
+    it "lograEstabilizar caso verdadero" $ do
+      lograEstabilizar [huesudo] (reduceFatFast 30) `shouldBe` True
+    it "lograEstabilizar caso falso" $ do
+      lograEstabilizar [huesudo] (reduceFatFast 1) `shouldBe` False
+    it "potenciaIdeal huesudo" $ do
+      potenciaIdeal [huesudo] `shouldBe` 29
+    it "potenciaIdeal cerebro" $ do
+      potenciaIdeal [bincenterrata] `shouldBe` 1
